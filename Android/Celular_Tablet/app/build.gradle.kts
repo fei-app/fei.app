008852b6e3e724915047be48716704a41fe29ea1
GMB8281 <32021180-GMB8281@users.noreply.gitlab.com>
2026-04-02 23:54:02 -0300

    Remoção de dependências desnecessárias.

--- Android/Celular_Tablet/app/build.gradle.kts
+++ Android/Celular_Tablet/app/build.gradle.kts
@@ -35,20 +35,15 @@ android {
 
 dependencies {
 
-    implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.webkit)
     implementation(libs.core.ktx)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
     implementation (libs.jsoup)
     implementation (libs.gson)
     implementation (libs.work.runtime.ktx)
-    implementation (libs.guava)
     implementation (libs.glide)
     annotationProcessor (libs.compiler)
     implementation (libs.swiperefreshlayout)
