7a9d39f49137937a73e3f32db06e2c8a671c838c
GMB8281 <32021180-GMB8281@users.noreply.gitlab.com>
2026-04-10 21:11:47 -0300

    1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.1"
         buildConfigField("String", "GITLAB_PAT", "\"${project.findProperty("GITLAB_PAT") ?: ""}\"")
     }
 
