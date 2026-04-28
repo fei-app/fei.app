d7817e6195a6e7c01eeb7e2076d75c434446223c
GMB8281 <32021180-GMB8281@users.noreply.gitlab.com>
2026-04-28 17:08:16 -0300

    Reorganização do repositório

---
+++ Android/app/build/generated/source/buildConfig/debug/com/marinov/openfei/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.marinov.openfei;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.marinov.openfei";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.5";
+  // Field from default config.
+  public static final String GITLAB_PAT = "glpat-pch45FJA8JRwKzbcBLnlPmM6MQpvOjEKdTpseHVjbg8.01.171sj2puc";
+}
