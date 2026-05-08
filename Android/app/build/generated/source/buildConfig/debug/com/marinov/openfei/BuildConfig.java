5933f107635c91f7c3d8461682353c63f67147dc
GMB8281 <32021180-GMB8281@users.noreply.gitlab.com>
2026-05-08 20:58:06 -0300

    2.1

--- Android/app/build/generated/source/buildConfig/debug/com/marinov/openfei/BuildConfig.java
+++ Android/app/build/generated/source/buildConfig/debug/com/marinov/openfei/BuildConfig.java
@@ -8,7 +8,7 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.marinov.openfei";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1.6_BETA";
+  public static final String VERSION_NAME = "2.0";
   // Field from default config.
   public static final String GITLAB_PAT = "glpat-pch45FJA8JRwKzbcBLnlPmM6MQpvOjEKdTpseHVjbg8.01.171sj2puc";
 }
