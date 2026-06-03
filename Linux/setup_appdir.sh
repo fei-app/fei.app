b5bf72a282769852895a85e771ffba6d9f56a00d
GMB8281 <32021180-GMB8281@users.noreply.gitlab.com>
2026-06-03 20:27:19 -0300

    2.4.1

--- Linux/setup_appdir.sh
+++ Linux/setup_appdir.sh
@@ -88,5 +88,5 @@ EOF
 chmod +x "$APPDIR/AppRun"
 echo "=== AppDir estruturado com sucesso e protegido contra atualizações! ==="
 
-ARCH=x86_64 ./appimagetool --no-appstream AppDir OpenFEI-1.0.2-x86_64.AppImage
+ARCH=x86_64 ./appimagetool --no-appstream AppDir OpenFEI-2.4.1-x86_64.AppImage
 echo "=== AppImage gerada! ==="
